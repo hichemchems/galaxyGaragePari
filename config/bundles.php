400e4aa965beb7ea6ec87cc13dd7f0fcc26ecf91
henocsombi <henoc.sombi@gmail.com>
2024-04-22 19:21:57 +0200

    Update to Doctrine MongoDB ODM Bundle 5.0
    This commit updates the Doctrine MongoDB ODM Bundle to version 5.0, which includes several changes such as:
    - Replace deprecated

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
-
 ];
